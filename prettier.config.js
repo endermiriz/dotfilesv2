2f5cbb3c00d9efd9e90ae8ef31a6598e749da4f3
TeomanM <teomanmdr@protonmail.com>
2024-08-17 06:22:03 +0300

    chore: Update submodules and formatting feat: Add prettier config waybar: Implement water animations

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+	arrowParens: "always",
+	bracketSpacing: true,
+	endOfLine: "lf",
+	htmlWhitespaceSensitivity: "css",
+	insertPragma: false,
+	singleAttributePerLine: false,
+	bracketSameLine: true,
+	jsxSingleQuote: false,
+	printWidth: 140,
+	proseWrap: "preserve",
+	quoteProps: "as-needed",
+	requirePragma: false,
+	semi: true,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "es5",
+	useTabs: true,
+	embeddedLanguageFormatting: "auto",
+};
+
+module.exports = config;
